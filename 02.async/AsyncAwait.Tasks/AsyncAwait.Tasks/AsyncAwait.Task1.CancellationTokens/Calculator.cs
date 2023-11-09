68ad573ae4140a058a20cb4a340438be3bcaad93
Victoria Vasyltsiv <victoria_vasyltsiv@epam.com>
2023-11-09 23:57:12 +0200

    Initial commit

---
+++ 02.async/AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens;
+
+internal static class Calculator
+{
+    // todo: change this method to support cancellation token
+    public static Task<long> Calculate(int n, CancellationToken token)
+    {
+        long sum = 0;
+
+        checked
+        {
+            for (var i = 0; i < n; i++)
+            {
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+                sum = sum + (i + 1);
+                Thread.Sleep(10);
+            }
+        }        
+
+        return Task.FromResult(sum);
+    }
+}
