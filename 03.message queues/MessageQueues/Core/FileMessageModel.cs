23ac0861d2d11e61c2325fb3612ab6b440d04659
Victoria Vasyltsiv <victoria_vasyltsiv@epam.com>
2023-11-16 23:51:20 +0200

    Initial commit

---
+++ 03.message queues/MessageQueues/Core/FileMessageModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Core
+{
+    public class FileMessageModel
+    {
+        public Guid SequenceId { get; set; }
+        public string Content { get; set; }
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+
+        public static FileMessageModel operator +(FileMessageModel messageA, FileMessageModel messageB) => new FileMessageModel()
+        {
+            SequenceId = messageA.SequenceId,
+            Content = messageA.Content + messageB.Content,
+            FileName = messageA.FileName,
+            Extension = messageA.Extension,
+        };
+    }
+}
