22a69302a37868260034a3ed092eeb773010bb8a
Victoria Vasyltsiv <victoria_vasyltsiv@epam.com>
2023-11-23 00:20:25 +0200

    Remove module 2

--- 02.async/AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Headers/CustomHttpHeaders.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AsyncAwait.Task2.CodeReviewChallenge.Headers;
-
-// Shouldn't it be static
-//public class CustomHttpHeaders
-public static class CustomHttpHeaders
-{
-    public const string TotalPageVisits = "TotalPageVisits";
-}
--- 03.message queues/MessageQueues/Core/FileMessageModel.cs
+++ 03.message queues/MessageQueues/Core/FileMessageModel.cs
@@ -6,13 +6,5 @@
         public string Content { get; set; }
         public string FileName { get; set; }
         public string Extension { get; set; }
-
-        public static FileMessageModel operator +(FileMessageModel messageA, FileMessageModel messageB) => new FileMessageModel()
-        {
-            SequenceId = messageA.SequenceId,
-            Content = messageA.Content + messageB.Content,
-            FileName = messageA.FileName,
-            Extension = messageA.Extension,
-        };
     }
 }
